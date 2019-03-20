0ce1827bee1a2a5c8d5310633d1a7281e9013b3b
Alexey Bondarenko <alexe23b@gmail.com>
2019-03-20 22:45:37 +0200

    Add files via upload
    Create project

---
+++ task.loc/www/index.php
@@ -0,0 +1,56 @@
+﻿<!DOCTYPE html>
+<head>
+		
+	<title>Тестовое задание</title>
+	
+</head>
+<body>
+	<form method="POST">
+        <input type="text" name="name_folder" maxlength="20" placeholder="Введите название папки" />
+        <input type="submit" name="creation_folder" value="Создать папку" />
+    </form>
+
+    <?php
+        # Если кнопка нажата
+        if( isset( $_POST['creation_folder'] ) )
+        {
+        	if($_POST['name_folder'] == null){
+        		echo "Вы не ввели имя папки!!!";
+        	}
+        	else{
+            @mkdir($_POST['name_folder'], 0700);
+        
+       
+	        $a = $_POST['name_folder'];
+	        $b = "./". $a. "/";
+	    	echo "Your folder is". $b;
+	    	$filename = 'dir.txt';
+			$text = $b;
+			//записываем текст в файл
+			file_put_contents($filename, $text);
+			}
+        }  
+    ?>
+	<form action="" method="POST" enctype="multipart/form-data">
+		<input type="file" name="uploadfile">
+		<input type="submit" name="upload" value="Загрузить">
+	</form>
+	<?php
+		if( isset( $_POST['upload'] ) )
+        { 
+	    // Каталог, в который мы будем принимать файл:
+		$filename = 'dir.txt';    	
+		$uploaddir = file_get_contents($filename);
+		$uploadfile = $uploaddir.basename($_FILES['uploadfile']['name']);
+
+		// Копируем файл из каталога для временного хранения файлов:
+		if (copy($_FILES['uploadfile']['tmp_name'], $uploadfile))
+		{
+		echo "<h3>Файл успешно загружен на сервер</h3>";
+		}
+		else { echo "<h3>Ошибка! Не удалось загрузить файл на сервер!</h3>"; exit; }
+		
+		}
+	?>
+</body>
+</html>
