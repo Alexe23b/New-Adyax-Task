5e390ce22484e4e965bf6c939366f4e1632270c0
alexe23b <alexe23b@gmail.com>
2019-03-21 20:55:44 +0200

    Объединил PHP код

--- task.loc/www/index.php
+++ task.loc/www/index.php
@@ -5,13 +5,8 @@
 	
 </head>
 <body>
-	<form method="POST">
-        <input type="text" name="name_folder" maxlength="20" placeholder="Введите название папки" />
-        <input type="submit" name="creation_folder" value="Создать папку" />
-    </form>
-
-    <?php
-        # Если кнопка нажата
+	 <?php
+       
         if( isset( $_POST['creation_folder'] ) )
         {
         	if($_POST['name_folder'] == null){
@@ -26,24 +21,19 @@
 	    	echo "Your folder is". $b;
 	    	$filename = 'dir.txt';
 			$text = $b;
-			//записываем текст в файл
+			
 			file_put_contents($filename, $text);
 			}
         }  
-    ?>
-	<form action="" method="POST" enctype="multipart/form-data">
-		<input type="file" name="uploadfile">
-		<input type="submit" name="upload" value="Загрузить">
-	</form>
-	<?php
+
 		if( isset( $_POST['upload'] ) )
         { 
-	    // Каталог, в который мы будем принимать файл:
+	    
 		$filename = 'dir.txt';    	
 		$uploaddir = file_get_contents($filename);
 		$uploadfile = $uploaddir.basename($_FILES['uploadfile']['name']);
 
-		// Копируем файл из каталога для временного хранения файлов:
+		
 		if (copy($_FILES['uploadfile']['tmp_name'], $uploadfile))
 		{
 		echo "<h3>Файл успешно загружен на сервер</h3>";
@@ -59,5 +49,16 @@
 		
 		}
 	?>
+	<form method="POST">
+        <input type="text" name="name_folder" maxlength="20" placeholder="Введите название папки" />
+        <input type="submit" name="creation_folder" value="Создать папку" />
+    </form>
+
+   
+	<form action="" method="POST" enctype="multipart/form-data">
+		<input type="file" name="uploadfile">
+		<input type="submit" name="upload" value="Загрузить">
+	</form>
+	
 </body>
 </html>
