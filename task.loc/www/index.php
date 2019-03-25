109973aab4ff9ac30599da3c0775ce1210bf7527
alexe23b <alexe23b@gmail.com>
2019-03-25 10:44:53 +0200

    Виправлені граматичні помилки у повідомленнях

--- task.loc/www/index.php
+++ task.loc/www/index.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 	<?php
-		function listing($filename){
+		function listing($filename) {
 			$uploaddir = file_get_contents($filename);
 			$hdl = opendir($uploaddir);
 			while ($file = readdir($hdl)) {
@@ -15,26 +15,25 @@
 				} 
 			}
 			closedir($hdl);
-
+			echo ("<form method=post>");
 			if (sizeof($c)>0) {
 				asort($c);
 				foreach ($c as $k) {
 					$full=$uploaddir. "/". $k;
-					echo ("<input name=fl[] value=$k type=checkbox>");
 					echo ("<a href=$full>$k</a>");
-				
-					echo " размер: ".filesize($full). " bytes"; 
-					echo ("<br>");
+					echo " розмір: ".filesize($full). " bytes"; 
+					echo ("<br>");					
 				}
 			}
 			else {
 				echo "Ваша папка порожня, додайте файли";
+				echo ("<br>");
 			}
-
 		}
 
-       
-        if( isset( $_POST['creation_folder'] ) ) {
+       	$filename = 'dir.txt';
+
+       	if(isset($_POST['creation_folder'])) {
         	if($_POST['name_folder'] == null){
         		echo "Ви не ввели ім'я' папки!!!";
         	}
@@ -45,7 +44,7 @@
 	        	$b = "./". $a. "/";
 	    		echo "Ваша робоча папка - ". $b;
 	    		echo ("<br>");
-	    		$filename = 'dir.txt';
+	    		
 				$text = $b;
 			
 				file_put_contents($filename, $text);
@@ -56,14 +55,14 @@
 			}
         }  
 
-		if( isset( $_POST['upload'] ) ) { 
+		if(isset($_POST['upload'])){ 
 	    
-			$filename = 'dir.txt';    	
+				
 			$uploaddir = file_get_contents($filename);
 			$uploadfile = $uploaddir.basename($_FILES['uploadfile']['name']);
 
 		
-			if (copy($_FILES['uploadfile']['tmp_name'], $uploadfile)) {
+			if (copy($_FILES['uploadfile']['tmp_name'], $uploadfile)){
 				echo "<h3>Файл успешно загружен на сервер</h3>";
 			}
 			else { 
@@ -71,9 +70,25 @@
 			}
 
 			listing($filename);
-
-			
 		}
+
+		if(isset($_POST['del'])){
+
+			$dir = file_get_contents($filename);
+			$files1 = array_diff(scandir($dir), array('..', '.'));
+			//print_r($files1);
+			
+			echo '<form name="fom" action="delete.php?filename=$filename">';
+			foreach($files1 as $k){
+			    echo '<input type="radio" name="'.$k.'" value="'.$k.'">'.'<label for="'.$k.'">'.$k.'</label>'.'<br>';
+			    }
+			    echo 'Позначьте файлы для видалення:';
+			echo '<input type="submit" value="Видалити вибрані файли" >';
+			echo '</form>';
+		}
+
+		
+		
 	?>
 	<form method="POST">
         <input type="text" name="name_folder" maxlength="20" placeholder="Введіть імя робочої папки" />
@@ -85,6 +100,11 @@
 		<input type="file" name="uploadfile">
 		<input type="submit" name="upload" value="Завантажити">
 	</form>
+
+	<form method="POST">
+        <input type="submit" name="del" value="Видалити файли" />
+    </form>
+	
 	
 </body>
 </html>
